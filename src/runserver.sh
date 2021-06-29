f74d1fcd2035586b0d5ee709bfc49b308ed6cc17
shahriarsohan <sohanmock@gmail.com>
2021-06-29 16:17:34 +0600

    nginx updated

--- src/runserver.sh
+++ src/runserver.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
 cd /var/app
 export PYTHONPATH=/var/app;$PYTHONPATH
+echo "Apply database migrations"
 python manage.py migrate --noinput
-python manage.py runserver 0.0.0.0:8000
+echo "Starting server"
+gunicorn cosmetica.wsgi --bind 0.0.0.0:8000
