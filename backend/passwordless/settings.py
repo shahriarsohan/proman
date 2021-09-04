af422693b7da27615ccb8c1b7d004771083bcc4a
shahriarsohan <sohanmock@gmail.com>
2021-09-04 16:02:17 +0600

    wishlist and profile updated

--- backend/passwordless/settings.py
+++ backend/passwordless/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 's*wkso3p2o%olg@gy^&^qq(t(4$u=zn^-#4%ase+^00!5v^5(_'
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '*', 'http://promantest-env.eba-u7qpm2r2.ap-south-1.elasticbeanstalk.com']
